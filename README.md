49099d6386f6ebca4c96fd579840b63a0f1bf99b
Mark Paspirgilis <external_github@power-of-m.de>
2018-04-23 09:47:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hashcracking.de
+
+## Core-Submodule:
+git submodule add git@github.com:MarkPaspirgilis/HTML42-Xtreme-Webframework-2.git xtreme
---
+++ components/html_head.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8" />
+
+<title>Hashcracking.de</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<meta name="theme-color" content="#69D269"/>
+
+<link href="<?= "assets/css/styles" . (ENV == 'live' ? '.min' : '') . ".css" ?>" rel="stylesheet" type="text/css" />
