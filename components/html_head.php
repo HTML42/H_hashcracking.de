b0badb2b134f7da5a18785751fcd28434a9ef548
Mark Paspirgilis <mark-paspirgilis@web.de>
2018-05-22 23:15:47 +0200

    .

--- components/html_head.php
+++ components/html_head.php
@@ -2,6 +2,8 @@
 
 <title>Hashcracking.de</title>
 
+<meta name="google-site-verification" content="Na7oo1gPGfYpOjLXPLgqVWJdPI3PDvEbE5sMjoFThTA" />
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <meta name="theme-color" content="#111111"/>
